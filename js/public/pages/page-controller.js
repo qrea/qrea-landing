9e45f9c0cb38a5138dc11491b5385fe61a8536f5
Pierre Bourdu <pierre@qrea.io>
2016-12-29 20:33:35 +0100

    v2 en typescript

--- js/public/pages/page-controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('qreaPublic')
-    .controller('PageCtrl',
-	['$scope', '$state', '$log', '$rootScope', '$timeout', '$document',
-        function ($scope, $state, $log, $rootScope, $timeout, $document)
-         {
-
-
-
-}]);
